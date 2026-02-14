ad0f69c57685b3cf9689bd8e39f16da04eb84458
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2026-02-14 16:38:10 +0000

    Fix indentation for hpge construction in core.py

--- src/pygeomhades/core.py
+++ src/pygeomhades/core.py
@@ -174,10 +174,6 @@ def construct(
     pv = _place_pv(holder_lv, "holder_pv", cavity_lv, reg, z_in_mm=z_pos)
     reg.addVolumeRecursive(pv)
 
-    # construct the hpge
-    detector_lv = make_hpge(hpge_meta, name=hpge_meta.name, registry=reg)
-    detector_lv.pygeom_color_rgba = [0.33, 0.33, 0.33, 1.0]
-    
     # construct the hpge, for now do not allow cylindrical asymmetry
     detector_lv = make_hpge(
         hpge_meta, name=hpge_meta.name, registry=reg, allow_cylindrical_asymmetry=False
